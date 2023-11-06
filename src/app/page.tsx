d95a8774d09e086243c5c3530543505b2ab495bd
Shivansh Shekhar <shivanshshekhar11@gmail.com>
2023-11-06 23:14:19 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
   });
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-15">
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <Link href="/signup">Sign Up</Link>
         <Link href="/login" style={{ marginLeft: "5em" }}>
